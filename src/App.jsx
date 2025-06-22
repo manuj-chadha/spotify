9f5912ca0b7da8135c5cb8d0ce5e094fe0fcdb1b
Manuj <manujchadha7777@gmail.com>
2025-06-22 16:47:01 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ const App = () => {
               <Route path="/top-artists" element={<TopArtists />} />
               <Route path="/top-charts" element={<TopCharts />} />
               <Route path="/around-you" element={<AroundYou />} />
-
               <Route path="/artists/:id" element={<ArtistDetails />} />
               <Route path="/songs/:songid" element={<SongDetails />} />
               <Route path="/search/:searchTerm" element={<Search />} />
@@ -30,13 +29,12 @@ const App = () => {
             <TopPlay />
           </div>
         </div>
-
       </div>
 
       {activeSong?.title && (
-      <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
-        <SongPlay />
-      </div>
+        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+          <MusicPlayer />
+        </div>
       )}
     </div>
   );
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
 import { selectGenreListId } from '../redux/features/playerSlice';
-import { useFetchSongsByGenreQuery } from '../redux/services/shazamCore';
+import { useGetSongsByGenreQuery } from '../redux/services/shazamCore';
+import { genres } from '../assets/constants';
 
 const genres = [
   'POP',
@@ -36,22 +37,20 @@ const Discover = () => {
 
   if (error) return <Error />;
 
+  const songs = location.pathname.startsWith('/search') ? data.map((song) => song.track) : data;
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white text-left">Discover {genreListId || 'pop'}</h2>
+        <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
 
         <select
           onChange={(e) => dispatch(selectGenreListId(e.target.value))}
           value={genreListId || 'pop'}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
-          {genres.map((genre) => (
-            <option
-              value={genre}
-            >{genre}
-            </option>
-          ))}
+          {genres.map((genre) => <option value={genre.value}>{genre.title}</option>)}
         </select>
       </div>
 
